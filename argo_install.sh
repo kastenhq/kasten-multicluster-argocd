4628661b45d132a83548f06e67624caf48fc9ce8
Full Aware <fullaware@users.noreply.github.com>
2024-09-03 14:05:06 -0400

    initial

--- argo_install.sh
+++ argo_install.sh
@@ -1,9 +1,5 @@
-# Deploying multiple applicationsets across multiple clusters
-
-
-```
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 argocd admin initial-password -n argocd
-```
+
