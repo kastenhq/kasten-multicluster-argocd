33aa35d0f8cd81360da98b7bfeac4bcac64f5424
Full Aware <fullaware@users.noreply.github.com>
2024-09-17 11:57:03 -0400

    renamed to argocd-kasten and using envsubst

--- argo_bootstrap.sh
+++ argo_bootstrap.sh
@@ -1,6 +1,6 @@
 argocd app create apps \
     --dest-namespace argocd \
     --dest-server https://kubernetes.default.svc \
-    --repo https://github.com/fullaware/argocd.git \
+    --repo https://github.com/fullaware/argocd-kasten.git \
     --path appsets  
 argocd app sync apps  
